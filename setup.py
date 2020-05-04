8ffddff09c8d76947ae8a6dc1bb80fa8900b727a
Christoph Roedig <chris@roedig.us>
2020-05-04 08:34:58 -0400

    bump version for tag

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="enlighten-croedig", # Replace with your own username
-    version="0.0.1",
+    version="0.1.0",
     author="Christoph A. Roedig",
     author_email="chris@roedig.us",
     description="Simple API client to easily extract data from Enphase Enlighten",
